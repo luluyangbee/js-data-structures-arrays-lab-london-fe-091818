af22ce39eb7f8714732e93bf96ab756b01aec08c
Lulu Yang <lulu.yang@wework.com>
2018-10-24 09:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(drivers) {
+  drivers.push("Ralph")
+  return drivers
+}
+
+function destructivelyPrependDriver(name) {
+  unshift
+}
+
+function destructivelyRemoveLastDriver() {
+  
+}
+
+function destructivelyRemoveFirstDriver() {
+  
+}
+
+function appendDriver(name) {
+  
+}
+
+function prependDriver(name) {
+  
+}
+
+function removeLastDriver() {
+  
+}
+
+function removeFirstDriver() {
+  
+}
+
